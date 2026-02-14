8d8b982fcd75054f15d6dc881cb6c3c599d6bc29
Shane Quigley <quigles@gmail.com>
2026-02-14 02:33:33 +0000

    Restore tailscale and ssh post-install scripts - tte was the real culprit

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,3 +1,5 @@
 run_logged $OMARCHY_INSTALL/post-install/pacman.sh
+run_logged $OMARCHY_INSTALL/post-install/tailscale.sh
+run_logged $OMARCHY_INSTALL/post-install/remote-access.sh
 source $OMARCHY_INSTALL/post-install/allow-reboot.sh
 source $OMARCHY_INSTALL/post-install/finished.sh
